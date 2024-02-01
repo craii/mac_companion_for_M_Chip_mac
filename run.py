fea430e357ea82edf114e26e4dbf3a2f45040adb
craii <dios-c@hotmail.com>
2024-02-01 16:04:55 +0800

    1
    1

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import sys
 from pathlib import Path
+sys.path.append(f"{Path(__file__).resolve().parent.parent}")
 
 os.system(f"{Path(__file__).resolve().parent}/venv/bin/python App.py")
