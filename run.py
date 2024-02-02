58665afd3998437975ba948506a7bdc51327323a
craii <dios-c@hotmail.com>
2024-02-02 11:05:29 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import os
 import sys
-from pathlib import Path
-sys.path.append(f"{Path(__file__).resolve().parent.parent}")
+# from pathlib import Path
+# sys.path.append(f"{Path(__file__).resolve().parent.parent}")
 
-os.system(f"{Path(__file__).resolve().parent}/venv/bin/python App.py")
+# os.system(f"{Path(__file__).resolve().parent}/venv/bin/python App.py")
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+# print(file_dir, type(file_dir))
+os.system(f"{file_dir}/venv/bin/python App.py")
